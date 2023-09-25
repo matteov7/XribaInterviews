390a6cff82c6885797c58606eb563f2ea2b8090e
matteo.venturi <m.venturi@xriba.com>
2023-09-25 18:02:05 +0200

    Add project files.

---
+++ XribaInterviews/AlgTest/TextTransformer.cs
@@ -0,0 +1,30 @@
+﻿namespace XribaInterviews.AlgTest
+{
+    /**
+     * Fill the missing methods and run tests.
+     **/
+
+    public class TextTransformer : ITextTransformer
+    {
+        public string? Text { get; }
+
+        public TextTransformer(string? word)
+        {
+        }
+
+        public bool IsPalindrome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSpaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
