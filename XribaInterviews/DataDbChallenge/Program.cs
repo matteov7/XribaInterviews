49b4dcd064c05c3b669c531aef1ff58f537fe96b
matteo.venturi <m.venturi@xriba.com>
2023-10-02 10:28:28 +0200

    added Main

---
+++ XribaInterviews/DataDbChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XribaInterviews.DataDbChallenge.Models;
+
+namespace XribaInterviews.DataDbChallenge
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            INorthwindReportService dbService = null!;
+        }
+    }
+}
