bf2e4e9e075f677fc3a459a9fcf974a0722f9118
matteo.venturi <m.venturi@xriba.com>
2023-10-03 13:19:45 +0200

    new exercise

---
+++ XribaInterviews/NameScore/INameScore.cs
@@ -0,0 +1,12 @@
+﻿namespace XribaInterviews.NameScore
+{
+    public interface INameScore
+    {
+        /// <summary>
+        /// Read a sequence of first names and calculate the total score of all names. 
+        /// </summary>
+        /// <param name="dataStream">stream to read names sequence (sequence of names comma separated)</param>
+        /// <returns>the total score calculated by this logic: https://projecteuler.net/problem=22</returns>
+        Task<int> GetTotalScoreAsync(Stream dataStream);
+    }
+}
---
+++ XribaInterviews/NameScore/NameScoreTest.cs
@@ -0,0 +1,21 @@
+﻿namespace XribaInterviews.NameScore
+{
+    public class NameScoreTest
+    {
+        [Theory]
+        [InlineData("0022_names.txt", 871198282)]
+        [InlineData("0022_names_2.txt", 222987815872)]
+        public async Task GetTotalScore_Test(string filename, ulong expectedResult)
+        {
+            INameScore nameScore = GetInstance();
+            using var s = File.OpenRead($"./NameScore/{filename}");
+            var result = await nameScore.GetTotalScoreAsync(s);
+            Assert.Equal(expectedResult, (ulong)result);
+        }
+
+        private INameScore GetInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
